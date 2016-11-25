7a5dd8c361b73482c5e9ae2c5b8de6b1789605a9
tychen <tychen@apex.sjtu.edu.cn>
2016-11-25 13:20:45 +0800

    label index args

--- bin/labelindex.cpp
+++ bin/labelindex.cpp
@@ -1,23 +1,53 @@
 #include <cstdio>
+#include <cstring>
 
 typedef unsigned int uint;
 
+const size_t MAXBUFF = 256;
+
+struct Args {
+	const char *sourcefile;
+	const char *targetfile;
+};
+
+void parse_args(Args &args, int argc, char *argv[]) {
+	bool is_sourcefile = false;
+	bool is_targetfile = false;
+
+	for (int i = 1; i < argc; ++i) {
+		if (!strcmp(argv[i], "--help")) {
+			throw 0;
+		} else if (!is_sourcefile) {
+			args.sourcefile = argv[i];
+			is_sourcefile = true;
+		} else if (!is_targetfile) {
+			args.targetfile = argv[i];
+			is_targetfile = true;
+		}
+	}
+	if (!is_sourcefile) throw 0;
+	if (!is_targetfile) throw 0;
+}
+
 int main(int argc, char *argv[]) {
-	if (argc < 3) {
-		fprintf(stderr, "No enough arguments.\n");
-		return 1;
+	Args args;
+	try {
+		parse_args(args, argc, argv);
+	} catch (int err) {
+		fprintf(stderr, "usage: sourcefile targetfile\n");
+		return 0;
 	}
 
-	FILE *sourcefp = fopen(argv[1], "r");
-	FILE *targetfp = fopen(argv[2], "wb");
+	FILE *s = fopen(args.sourcefile, "r");
+	FILE *t = fopen(args.targetfile, "wb");
 
 	uint x;
-	while (fscanf(sourcefp, "%u", &x) != EOF) {
-		fwrite(&x, sizeof(uint), 1, targetfp);
+	while (fscanf(s, "%u", &x) != EOF) {
+		fwrite(&x, sizeof(uint), 1, t);
 	}
 
-	fclose(sourcefp);
-	fclose(targetfp);
+	fclose(s);
+	fclose(t);
 
 	return 0;
 }
