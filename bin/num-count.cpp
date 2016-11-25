8c03bfb92d20b9cadef2126dbf61bc770ddc956d
tychen <tychen@apex.sjtu.edu.cn>
2016-11-25 13:37:52 +0800

    num-count

--- bin/num-count.cpp
+++ bin/num-count.cpp
@@ -1,14 +1,26 @@
 #include <cstdio>
+#include <cstring>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
 
 typedef unsigned int uint;
 
 const uint MAXNUM = 1 << 25;
 
-const char *sourcefile;
-const char *targetfile;
+struct CntPair {
+	int x;
+	uint cnt;
 
-FILE *sourcefp, *targetfp;
-uint cnt[MAXNUM];
+	CntPair(int _x, uint _cnt) : x(_x), cnt(_cnt) {
+	}
+
+	bool operator<(const CntPair& r) {
+		return x < r.x;
+	}
+};
+
+std::unordered_map<int, uint> m;
 
 void up(const char *file) {
 	FILE *fp = fopen(file, "r");
@@ -16,34 +28,67 @@ void up(const char *file) {
 	while (fscanf(fp, "%d", &x) != EOF) {
 		if (x < 0) {
 			continue;
-		} else if (x >= MAXNUM) {
-			fprintf(stderr, "Number %d out of bound.\n", x);
-			throw 0;
 		}
-		++cnt[x];
+		if (m.find(x) == m.end()) {
+			m[x] = 1;
+		} else {
+			++m[x];
+		}
 	}
 	fclose(fp);
 }
 
 void down(const char *file) {
+	std::vector<CntPair> v;
+	for (std::unordered_map<int, uint>::const_iterator it = m.begin(); it != m.end(); ++it) {
+		v.push_back(CntPair(it->first, it->second));
+	}
+
+	std::sort(v.begin(), v.end());
+
 	FILE *fp = fopen(file, "wb");
-	for (uint i = 0; i < MAXNUM; ++i) {
-		if (cnt[i]) {
-			fwrite(&i, sizeof(uint), 1, fp);
-			fwrite(cnt + i, sizeof(uint), 1, fp);
-		}
+	for (std::vector<CntPair>::const_iterator it = v.begin(); it != v.end(); ++it) {
+		fwrite(&it->x, sizeof(int), 1, fp);
+		fwrite(&it->cnt, sizeof(uint), 1, fp);
 	}
 	fclose(fp);
 }
 
+struct Args {
+	const char *sourcefile;
+	const char *targetfile;
+};
+
+void parse_args(Args &args, int argc, char *argv[]) {
+	bool is_sourcefile = false;
+	bool is_targetfile = false;
+
+	for (int i = 1; i < argc; ++i) {
+		if (!strcmp(argv[i], "--help")) {
+			throw 0;
+		} else if (!is_sourcefile) {
+			args.sourcefile = argv[i];
+			is_sourcefile = true;
+		} else if (!is_targetfile) {
+			args.targetfile = argv[i];
+			is_targetfile = true;
+		}
+	}
+	if (!is_sourcefile) throw 0;
+	if (!is_targetfile) throw 0;
+}
+
 int main(int argc, char *argv[]) {
-	if (argc < 3) {
-		fprintf(stderr, "No enough arguments.\n");
-		return 1;
+	Args args;
+	try {
+		parse_args(args, argc, argv);
+	} catch (int err) {
+		fprintf(stderr, "usage: sourcefile targetfile\n");
+		return 0;
 	}
 
-	up(argv[1]);
-	down(argv[2]);
+	up(args.sourcefile);
+	down(args.targetfile);
 
 	return 0;
 }
