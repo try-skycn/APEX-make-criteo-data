a882a28deafc0b68778b486675f6dd4962415c4e
tychen <tychen@apex.sjtu.edu.cn>
2016-11-25 19:03:35 +0800

    newline

--- bin/num-index.cpp
+++ bin/num-index.cpp
@@ -148,7 +148,7 @@ int main(int argc, char *argv[]) {
 		parse_args(args, argc, argv);
 	} catch (int err) {
 		fprintf(stderr, "usage: sourcefile targetfile --counter counterfile --featmap featmapfile --threshold threshold\n");
-		fprintf(stderr, "print: num of categories");
+		fprintf(stderr, "print: num of categories\n");
 		return 0;
 	}
 
