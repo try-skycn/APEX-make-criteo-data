a9707ab73ef14e6359155a1716ae8fa1c9244a77
Tianyao Chen <try.skycn@Gmail.com>
2017-03-21 18:39:59 +0800

    fix

--- .tool/util.py
+++ .tool/util.py
@@ -2,6 +2,6 @@ import sys
 
 def showenum(gen, start=1):
 	for i, x in enumerate(gen, start=start):
-		print("\033[K" + i, end='\r', flush=True, file=stderr)
+		print("\033[K" + str(i), end='\r', flush=True, file=stderr)
 		yield i, x
-	print("\033[K" + i, file=sys.stderr)
+	print("\033[K" + str(i), file=sys.stderr)
