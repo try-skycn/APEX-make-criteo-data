3dc517c1c40eac4f4aef8d91a23eb38cef579d0c
Tianyao Chen <try.skycn@Gmail.com>
2017-03-21 19:36:20 +0800

    modcate & modnum

---
+++ .tool/modcate.py
@@ -0,0 +1,11 @@
+import collections
+
+class Counter(collections.Counter):
+	def add(self, element):
+		super(Counter, self).update([element])
+
+	def index2cate(self, threshold):
+		return [None, ] + [x for x, cnt in self.most_common() if cnt >= threshold]
+
+def cate2index(lst):
+	return {x: i for i, x in enumerate(lst[1:], start=1)}
---
+++ .tool/modnum.py
@@ -0,0 +1,21 @@
+import collections
+
+class Counter(collections.Counter):
+	def add(self, element):
+		super(Counter, self).update([element])
+
+	def index2cate(self, threshold):
+		lst = list(self.items())
+		lst.sort(key=lambda x: x[0])
+
+		rst = [None, ]
+		l, tot = 0, 0
+		for x, cnt in lst:
+			tot += cnt
+			if tot >= threshold:
+				rst.append((l, x + 1))
+				l, tot = x + 1, 0
+		return rst
+
+def cate2index(lst):
+	return {j: i for i, (l, r) in enumerate(lst[1:], start=1) for j in range(l, r)}
