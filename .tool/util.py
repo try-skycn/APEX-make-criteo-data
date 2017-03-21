85d4fa033d37dfea0b81a6a798cd39f265a01701
Tianyao Chen <try.skycn@Gmail.com>
2017-03-21 18:46:16 +0800

    loop

--- .tool/util.py
+++ .tool/util.py
@@ -1,7 +1,8 @@
 import sys
 
-def enum(gen, start=1):
+def enum(gen, start=1, loop=100000):
 	for i, x in enumerate(gen, start=start):
-		print("\033[K" + str(i), end='\r', flush=True, file=sys.stderr)
+		if i == 1 or i % loop == 0:
+			print("\033[K" + str(i), end='\r', flush=True, file=sys.stderr)
 		yield i, x
 	print("\033[K" + str(i), file=sys.stderr)
